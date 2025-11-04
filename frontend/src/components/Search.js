02ca2bccf110a12eefc732fc727dd3e57109e588
SJD16 <javunam616@gmail.com>
2025-11-04 17:24:58 -0600

    C1

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -18,7 +18,7 @@ const Search = ({ word, setWord, handleSubmit }) => {
               </Col>
               <Col>
                 <Button variant="primary" type="submit">
-                  Search img
+                  Search XD
                 </Button>
               </Col>
             </Form.Row>
